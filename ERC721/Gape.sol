7d333d9232da1fdeb3abb1282b97d1a45193f377
Guocork <g1024536444@gmail.com>
2024-05-09 23:21:19 +0800

    完善解释Gape合约

--- ERC721/Gape.sol
+++ ERC721/Gape.sol
@@ -1,3 +1,8 @@
+/**
+    这里是对使用ERC721 写了一个铸造Gape nft的合约实践 代表了一个名为 "Gape" 的 NFT（非同质化代币）系列
+ */
+
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
@@ -6,6 +11,9 @@ import "./ERC721.sol";
 contract Gape is ERC721 {
     uint public MAX_APES = 10000; 
 
+    /**
+        这里是合约继承构造器的一种写法 继承父合约中构造器的写法 通过调用父合约的构造函数来初始化继承的属性和状态。
+     */
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
 
     function _baseURI() internal pure override returns(string memory) {
